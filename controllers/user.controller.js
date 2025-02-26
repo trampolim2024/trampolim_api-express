1d9e550ccef16f7d6d29d03354814476906fb616
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2025-02-26 18:07:52 -0300

    feat: user controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,56 @@
+import User from '../models/user.model.js';
+
+export const createUser = async (req, res, next) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json({ success: true, data: user });
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({ success: true, data: users });
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const getUserById = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ success: false, message: 'Usuário não encontrado' });
+    }
+    res.status(200).json({ success: true, data: user });
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const updateUser = async (req, res, next) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!updatedUser) {
+      return res.status(404).json({ success: false, message: 'Usuário não encontrado' });
+    }
+    res.status(200).json({ success: true, message: 'Usuário atualizado com sucesso!', data: updatedUser });
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) {
+      return res.status(404).json({ success: false, message: 'Usuário não encontrado' });
+    }
+    res.status(200).json({ success: true, message: 'Usuário excluído com sucesso!' });
+  } catch (e) {
+    next(e);
+  }
+};
