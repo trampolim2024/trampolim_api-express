dfe511ac44bbcebd1d170ed562e8b51ad562542d
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2025-02-26 14:33:55 -0300

    feat: add project route to the app.js

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ const app = express();
 
 import { PORT } from './config/env.js';
 
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import announcementRouter from './routes/announcement.routes.js';
+import projectRouter from './routes/project.routes.js';
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/announcements', announcementRouter);
+app.use('/api/v1/projects', projectRouter);
+
+
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 export default app; 
