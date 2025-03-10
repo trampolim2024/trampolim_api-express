a99e08b3b620c8cf3455d1c62a7cbea3a1ddba4a
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2025-03-10 13:40:35 -0300

    feat: add the authorize middleware to the route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,16 +1,17 @@
 import { Router } from 'express';
-import { 
-  getAllUsers, 
-  getUserById, 
-  updateUser, 
-  deleteUser 
+import {
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser
 } from '../controllers/user.controller.js';
+import authorize from '../middlewares/auth.middleware.js';
 
 const userRouter = Router();
 
 // Definindo as rotas para o CRUD de usuários
 userRouter.get('/', getAllUsers);
-userRouter.get('/:id', getUserById);
+userRouter.get('/:id', authorize, getUserById);
 userRouter.put('/:id', updateUser);
 userRouter.delete('/:id', deleteUser);
 
